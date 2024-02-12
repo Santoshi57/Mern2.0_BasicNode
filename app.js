885c7ff044f6773de02d5a41e1caf28720f2ea2f
Santoshi Chaulagain <santoshichaulagain57@gmail.com>
2024-02-12 08:44:39 +0545

    changes

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
+const mangoose = require('mongoose')
+
+
+const ConnectionString="mongodb+srv://santoshi:santoshi@cluster0.s5ezukk.mongodb.net/?retryWrites=true&w=majority"
 
 //alternative
 //const app = require('express')()
+
+ async function connectToDatabase(){
+   await mangoose.connect(ConnectionString)
+   console.log("Connectes to DB successfully")
+  }
+  connectToDatabase()
 
 
 app.get("/",(req,res)=>{
